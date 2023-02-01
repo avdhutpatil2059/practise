d4be6a0362768cb07cc8f522200cdb9637f4bc70
avdhutpatil2059 <avdhut2059@gmail.com>
2023-02-01 20:08:45 +0530

    substraction

--- Sub.java
+++ Sub.java
@@ -2,7 +2,7 @@ public class Sum{
       public static void main (String args[]) {
             int a = 5;
             int b = 10;
-            System.out.println("Sub is as follows"+(a-b));
+            System.out.println("Substraction is as follows"+(a-b));
       }
 
 }
